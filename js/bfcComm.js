7ab422d6aca11437aa6ca5e7b6d99878536cb485
kgckgc <kgckgc@naver.com>
2017-06-26 15:50:06 +0900

    Update bfcComm.js

--- js/bfcComm.js
+++ js/bfcComm.js
@@ -1,3 +1,14 @@
+/*===================================================================================================================
+ * @name: bfcComm.js
+ * @desc: Co
+ * @type: jQuery
+ * @author: Kim Gwang-Cheon (KGC), Korean
+ * @demo: http:/....
+ * @version: 1.0
+ * @requires jQuery x.x.x
+ * @since: 2017.06.26
+ * @see: https://github.com/dinbror/bpopup/blob/master/jquery.bpopup.js
+ *==================================================================================================================*/
 "use strict";
 
 
